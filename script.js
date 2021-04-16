966f2b8b5fa2eba82f7eba790a548c1e95b96b68
Josiahhicks555 <70174168+Josiahhicks555@users.noreply.github.com>
2021-04-16 11:42:19 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,79 @@
+alert("Welcome to my text Adventure");
+//charecter selection
+
+
+//Adventure
+const textElement = document.getElementById('text');
+
+
+//choice responses
+const optionButtonElement = document.getElementById('options');
+
+let changes = {}
+
+
+function start() {
+    changes = {}
+    
+}
+
+
+
+function text(changeText) {
+
+}
+
+
+function responses(options) {
+
+}
+
+const firstText = [{
+
+}
+]
+
+start()
+
+
+
+
+
+
+
+
+
+
+//start
+/*var option1 = 1;
+var option2 = 2;
+var option3 = 3;
+function MainFunction(){
+    var x, text;
+    input = document.getElementById("firstInput").value;
+    console.log(input);
+    document.getElementById("output").innerHTML = input + " <--- This is the input you gane.";
+
+    document.getElementById("storyImage").src ='';
+
+//story
+
+
+
+//outcomes 
+    if (x = option1){
+        var text = "you and your friend get lost and never find your way back";
+        document.getElementById("answer").innerHTML = text;
+
+    }else if (x = option2){
+        var text = "you and your friend fall through the tree and find a secret cave inside";
+        document.getElementById("answer").innerHTML = text;
+    }else (x = option3){
+        var text = "Your adventure ends";
+        document.getElementById("answer").innerHTML = text;
+        
+    }
+} */
+
+
+
