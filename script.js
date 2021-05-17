024dd27706fd60a896600df247e8be58d37a91fb
Josiahhicks555 <70174168+Josiahhicks555@users.noreply.github.com>
2021-05-17 09:42:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,381 @@
+//alert
 alert("Welcome to my text Adventure");
+
+var name = prompt("Please enter Ben or Tommy", "Whats your name");
+
+if (name == "Ben" || name == "Tommy") {
+  text = "You are now playing as " + name;
+} else {
+text = "I don't think thats your name"}
+
+//option var                 
+//var option1 = btn1;
+//var option2 = btn2;
+//var option3 = btn3;
+//var x = text;
+
 //charecter selection
 
+//#region Story
 
-//Adventure
-const textAdventure = document.getElementById('text');
+//part 1___________________________________________________
+if () {
+    text = "After you get up you notice that the cave has multiple different paths. Your friend thinks you should try going the middle path but you are not sure and decide to go..."
+
+}else {
+    text ="game over"
+}
+
+
+
+//part 2___________________________________________________
+if () {
+    text = "You and your friend decide to look around the room and discover a strange looking sword. You grab the sword and the portal suddenly lights. There is someone calling for help on the other side of the portal. What will you do?"
+
+}else {
+    text ="game over"
+}
+
+//part 3___________________________________________________
+if () {
+    text = "You look around and realize that everyone has health and stamina bars then seconds later you see someone yelling for help as they are being attacked by a large lion. What do you do?"
+
+}else {
+    text ="game over"
+}
+
+
+
+//part 4___________________________________________________
+if () {
+    text = "You manage to make a cut on the lion's paw and it runs away. You then ask the person where you are and who they were. They respond and say their name is Alias and they are currently in a field of danger beasts. You have no clue what Alias is talking about but you know it's not good. She asks you if you want to go back to her village for dinner to  thank you for saving her. What do you say?"
+
+}else {
+    text ="game over"
+}
+
+
+
+//part 5___________________________________________________
+if () {
+    text = "When you eat dinner and rest you decide to ask Alias for help finding your way back home and menchions the portal you walked through. Alias says she thinks she heard about a portal before and that it was located on a giant mountain. She offers to let you take a horse for faster and safe travel. What do you do?"
+
+}else {
+    text ="game over"
+}
+
+
+//part 6___________________________________________________
+if () {
+    text = "You and your friend start climbing the mountain. You start remembering about climbing the tree and falling in a hole and start worrying about falling again. Your friend tries to calm you down but you are not snapping out of it. Then out of nowhere Alias comes to make sure you made it safe. After you hear her you suddenly snapped out of it and decided to?"
+
+}else {
+    text ="game over"
+}
+
+
+
+//part 7___________________________________________________
+if () {
+    text = "You approached the portal but it wasn't active and you and your friend are trying to figure out how to light it but nothing you do is working. You get mad and throw your sword at the portal. It makes a noise and lights. You both get happy and walk through the portal you ended up back in the portal room. What do you do now?"
+
+}else {
+    text ="game over"
+}
+
+
+
+//part 8___________________________________________________
+if () {
+    text = "On your way back home you and your friend talk about how crazy that whole thing was, and start realizing how fun it was. You talk to your friend and decide to?"
+
+}else {
+    text ="game over"
+}
+
+
+
+//part 9___________________________________________________
+if () {
+    text = "You quickly ran back to the portal to try and close it but it was too late the cave was covered in danger beasts. But suddenly the portal closes your confusion till you realize you forgot the sword at the other portal and Alias ended up closing it on the other side. The danger beasts were soon killed by the military. You want to go back to thank Alias for saving everyone but there is no way to get back through. The portal then lights again and Alias walks through. What do you do?"
+
+}else {
+    text ="game over"
+}
+
+
+
+//part 10__________________________________________________
+if () {
+    text = "Congratulations on surviving this Text Adventure"
+
+}else {
+    text ="game over"
+}
+
+
+//#endregion
+
+
+//#region Story and outcome
+
+
+//part 1 story options_____________________________________
+if () {
+    btn1 = "keep walking and act like you didn't see it";
+    btn2 = "Climb the tree trying to get to the top";
+    btn3 = "go home and end your adventure";
+
+}else {
+    text = "ERROR";
+}
+
+//part 1 outcome___________________________________________
+if (text = btn1) {
+    text = "you and your friend get lost and never find your way back";
+
+} else if (text = btn2) {
+    text = "Fall through the tree and find a secret cave inside";
+}
+
+else {
+    text = "Your adventure ends";
+}
+
+
+
+//part 2 story options_____________________________________
+if () {
+    btn1 = "Left path";
+    btn2 = "middle path";
+    btn3 = "right path";
+
+}else {
+    text = "ERROR";
+}
+
+//part 2 outcome___________________________________________
+if (text = btn1) {
+    text = "your walking for what seems like forever not able to see anything you fall and your adventure ends";
+
+} else if (text = btn2) {
+    text = "your stuck in a boss fight that you were not prepared for and gets stuck fighting it";
+}
+
+else {
+    text = "you find a strange room with a portal you have never seen before";
+}
+
+
+
+//part 3 story options_____________________________________
+if () {
+    btn1 = "Take the sword and cut down the tree to go back home";
+    btn2 = "Go help the person on the other side of the portal";
+    btn3 = "Go back and try a different path";
+
+}else {
+    text = "ERROR";
+}
+
+//part 3 outcome___________________________________________
+if (text = btn1) {
+    text = "Your adventure ends";
+
+} else if (text = btn2) {
+    text = "you have been teleported to a new world";
+}
+
+else {
+    text = "The other paths were a trap and you died";
+}
+
+
+
+//part 4 story options_____________________________________
+if () {
+    btn1 = "Fight the lion to save the person";
+    btn2 = "Run away in fear";
+    btn3 = "Let them deal with the lion and go back through the portal";
+
+}else {
+    text = "ERROR";
+}
+
+//part 4 outcome___________________________________________
+if (text = btn1) {
+    text = "you are having a rough time finding a weak point to hit but are managing to keep fighting";
+
+} else if (text = btn2) {
+    text = "The lion started chasing you and kelled you";
+}
+
+else {
+    text = "you walked through the portal it closed when you were half way through and you were cut in half";
+}
+
+
+
+//part 5 story options_____________________________________
+if () {
+    btn1 = "Yes I would love too";
+    btn2 = "I can't I need to find my way back home";
+    btn3 = "Go to finish off the lion";
+
+}else {
+    text = "ERROR";
+}
+
+//part 5 outcome___________________________________________
+if (text = btn1) {
+    text = "you have learned a lot about their world and how everything there works";
+
+} else if (text = btn2) {
+    text = "you have been traveling for over 80 years and decide to give up and make a hut to live the rest of your life there";
+}
+
+else {
+    text = "you found the lion but it was with its pack and they killed you";
+}
+
+
+
+//part 6 story options_____________________________________
+if () {
+    btn1 = "Thank her and use the horse";
+    btn2 = "No Thanks";
+    btn3 = "Still have doubts about trusting her and go back to where the original portal was";
+
+}else {
+    text = "ERROR";
+}
+
+//part 6 outcome___________________________________________
+if (text = btn1) {
+    text = "you got to the mountain but the horse could climb it so you left it there";
+
+} else if (text = btn2) {
+    text = "it took you a few days to make it but you finally got to the mountain";
+}
+
+else {
+    text = "the lion came back with its Pack and killed you";
+}
+
+
+
+//part 7 story options_____________________________________
+if () {
+    btn1 = "Stay in the new world because you don't want to fall off the mountain";
+    btn2 = "Thank Alias for all her help and Climb the mountain to get home";
+    btn3 = "Go back to the original portal because you didn't trust Alias";
+
+}else {
+    text = "ERROR";
+}
+
+//part 7 outcome___________________________________________
+if (text = btn1) {
+    text = "Alias parents were glad to take you in and you stayed there the rest of your life";
+
+} else if (text = btn2) {
+    text = "you made it to the top and saw the portal";
+}
+
+else {
+    text = "you approached the portal you saw the lion dead and ran towards. it was a trap and you were attacked by an ogre and died";
+}
+
+
+
+//part 8 story options_____________________________________
+if () {
+    btn1 = "Go back and try a different path";
+    btn2 = "Cut down the tree";
+    btn3 = "Go back because you realize there was no escape";
+
+}else {
+    text = "ERROR";
+}
+
+//part 8 outcome___________________________________________
+if (text = btn1) {
+    text = "You died";
+
+} else if (text = btn2) {
+    text = "you were successful and started walking back home";
+}
+
+else {
+    text = "ou explained everything to Alias and her parents and they let you stay";
+}
+
+
+
+//part 9 story options_____________________________________
+if () {
+    btn1 = "Go find other portal rooms and explore other worlds";
+    btn2 = "Go back to Alias world";
+    btn3 = "Never do that again";
+
+}else {
+    text = "ERROR";
+}
+
+//part 9 outcome___________________________________________
+if (text = btn1) {
+    text = "you and your friend have been exploring for over 30 years now and traveled to 10 different worlds all so different then the others, but you end up dying after one of the portals put you in a volcano";
+
+} else if (text = btn2) {
+    text = "you traveled back and forth constantly and now both worlds have joined together to make peace";
+}
+
+else {
+    text = "you forgot to close the portal on the mountain and a bunch of danger beasts got through the portal and started causing mayhem in town";
+}
+
+
+
+//part 10 story options____________________________________
+if () {
+    btn1 = "Ask her what are you doing here";
+    btn2 = "Give her a hug and thank her for saving everyone";
+    btn3 = "Go back with her so the portal can stay open without danger beasts going through";
+
+}else {
+    text = "ERROR";
+}
+
+//part 10 outcome__________________________________________
+if (text = btn1) {
+    text = "she asks you to come with her to finish the danger beasts off. you decide to join her.";
+
+} else if (text = btn2) {
+    text = "you ended up going with her in the portal to fight the danger beasts and ended up getting married";
+}
+
+else {
+    text = "after they were all killed you decided to stay in the other world and live with Alias";
+}
+
+
+
+
+// end of game_____________________________________________
+if () {
+    alert("Congratulations on surviving this Text Adventure");
+}else {
+    text = "Error";
+}
+
+
+//#endregion
 
 
 //choice responses
-const optionButton = document.getElementById('options');
-
-let changes = {}
-
-
-function startAdventure() {
-    changes = {}
-    text(1)
-    
-}
 
 
 
-function text(changeText) {
-
-}
-
-
-function responses(options) {
-
-}
-
-const firstText = [{
-    id: 1, text:"You and your friend are walking through the woods. You start messing around when you see a hung tree next to a cliff. As you look at the tree you see something. What do you do ",
-    options:[
-    {
-        text:'keep walking and act like you did not see it',
-    },
-    {
-        text:'Climb the tree trying to get to the top',
-    },
-    {
-        text:'Or you can go home and end your adventure',
-    },
-]
-
-
-}
-]
-
-start()
-
-
-
-
-
-
-
-
-
-
-//start
-/*var option1 = 1;
-var option2 = 2;
-var option3 = 3;
-function MainFunction(){
-    var x, text;
-    input = document.getElementById("firstInput").value;
-    console.log(input);
-    document.getElementById("output").innerHTML = input + " <--- This is the input you gane.";
-
-    document.getElementById("storyImage").src ='';
-
-//story
-
-
-
-//outcomes 
-    if (x = option1){
-        var text = "you and your friend get lost and never find your way back";
-        document.getElementById("answer").innerHTML = text;
-
-    }else if (x = option2){
-        var text = "you and your friend fall through the tree and find a secret cave inside";
-        document.getElementById("answer").innerHTML = text;
-    }else (x = option3){
-        var text = "Your adventure ends";
-        document.getElementById("answer").innerHTML = text;
-        
-    }
-} */
 
 
 
